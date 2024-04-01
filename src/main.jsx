4b85ccceef2d10f08921802c10460438f4e7c59d
unknown <naimurrahman8392@gmail.com>
2024-04-01 17:30:00 +0600

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import {
   createBrowserRouter,
@@ -8,6 +7,9 @@ import {
 } from "react-router-dom";
 import Root from './components/Root/Root.jsx';
 import Home from './components/Home/Home.jsx';
+import Login from './components/Login/Login.jsx';
+import Register from './components/Register/Register.jsx';
+import HeroRegister from './components/HeroRegister/HeroRegister.jsx';
 const router = createBrowserRouter([
   {
     path: "/",
@@ -16,6 +18,19 @@ const router = createBrowserRouter([
       {
         path:'/',
         element:<Home></Home>
+      },
+      {
+        path: '/login',
+        element:<Login></Login>
+      },
+      {
+        path: '/register',
+        element:<Register></Register>
+      },
+      {
+        path:'/heroregister',
+        element:<HeroRegister></HeroRegister>
+    
       }
     ]
   },
